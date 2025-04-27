841712ea45cca9eb959cc7fdb0e8633e4cb32664
zhetian123 <167642733+zhetian123@users.noreply.github.com>
2025-04-27 18:21:27 +0800

    Add files via upload

---
+++ TIM/compressai/layers/layers.py
@@ -0,0 +1,365 @@
+# Copyright 2020 InterDigital Communications, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+from einops import rearrange
+
+# from .gdn import GDN
+
+
+class MaskedConv2d(nn.Conv2d):
+    r"""Masked 2D convolution implementation, mask future "unseen" pixels.
+    Useful for building auto-regressive network components.
+
+    Introduced in `"Conditional Image Generation with PixelCNN Decoders"
+    <https://arxiv.org/abs/1606.05328>`_.
+
+    Inherits the same arguments as a `nn.Conv2d`. Use `mask_type='A'` for the
+    first layer (which also masks the "current pixel"), `mask_type='B'` for the
+    following layers.
+    """
+
+    def __init__(self, *args, mask_type="A", **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if mask_type not in ("A", "B"):
+            raise ValueError(f'Invalid "mask_type" value "{mask_type}"')
+
+        self.register_buffer("mask", torch.ones_like(self.weight.data))
+        _, _, h, w = self.mask.size()
+        self.mask[:, :, h // 2, w // 2 + (mask_type == "B") :] = 0
+        self.mask[:, :, h // 2 + 1 :] = 0
+
+    def forward(self, x):
+        # TODO(begaintj): weight assigment is not supported by torchscript
+        self.weight.data *= self.mask
+        return super().forward(x)
+
+
+def conv3x3(in_ch, out_ch, stride=1):
+    """3x3 convolution with padding."""
+    return nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=stride, padding=1)
+
+
+def subpel_conv3x3(in_ch, out_ch, r=1):
+    """3x3 sub-pixel convolution for up-sampling."""
+    return nn.Sequential(
+        nn.Conv2d(in_ch, out_ch * r ** 2, kernel_size=3, padding=1), nn.PixelShuffle(r)
+    )
+
+
+def conv1x1(in_ch, out_ch, stride=1):
+    """1x1 convolution."""
+    return nn.Conv2d(in_ch, out_ch, kernel_size=1, stride=stride)
+
+
+class ResidualBlockWithStride(nn.Module):
+    """Residual block with a stride on the first convolution.
+
+    Args:
+        in_ch (int): number of input channels
+        out_ch (int): number of output channels
+        stride (int): stride value (default: 2)
+    """
+
+    def __init__(self, in_ch, out_ch, stride=2):
+        super().__init__()
+        self.conv1 = conv3x3(in_ch, out_ch, stride=stride)
+        self.leaky_relu = nn.LeakyReLU(inplace=True)
+        self.conv2 = conv3x3(out_ch, out_ch)
+        self.gdn = GDN(out_ch)
+        if stride != 1 or in_ch != out_ch:
+            self.skip = conv1x1(in_ch, out_ch, stride=stride)
+        else:
+            self.skip = None
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.leaky_relu(out)
+        out = self.conv2(out)
+        out = self.gdn(out)
+
+        if self.skip is not None:
+            identity = self.skip(x)
+
+        out += identity
+        return out
+
+
+class ResidualBlockUpsample(nn.Module):
+    """Residual block with sub-pixel upsampling on the last convolution.
+
+    Args:
+        in_ch (int): number of input channels
+        out_ch (int): number of output channels
+        upsample (int): upsampling factor (default: 2)
+    """
+
+    def __init__(self, in_ch, out_ch, upsample=2):
+        super().__init__()
+        self.subpel_conv = subpel_conv3x3(in_ch, out_ch, upsample)
+        self.leaky_relu = nn.LeakyReLU(inplace=True)
+        self.conv = conv3x3(out_ch, out_ch)
+        self.igdn = GDN(out_ch, inverse=True)
+        self.upsample = subpel_conv3x3(in_ch, out_ch, upsample)
+
+    def forward(self, x):
+        identity = x
+        out = self.subpel_conv(x)
+        out = self.leaky_relu(out)
+        out = self.conv(out)
+        out = self.igdn(out)
+        identity = self.upsample(x)
+        out += identity
+        return out
+
+
+class ResidualBlock(nn.Module):
+    """Simple residual block with two 3x3 convolutions.
+
+    Args:
+        in_ch (int): number of input channels
+        out_ch (int): number of output channels
+    """
+
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv1 = conv3x3(in_ch, out_ch)
+        self.leaky_relu = nn.LeakyReLU(inplace=True)
+        self.conv2 = conv3x3(out_ch, out_ch)
+        if in_ch != out_ch:
+            self.skip = conv1x1(in_ch, out_ch)
+        else:
+            self.skip = None
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.leaky_relu(out)
+        out = self.conv2(out)
+        out = self.leaky_relu(out)
+
+        if self.skip is not None:
+            identity = self.skip(x)
+
+        out = out + identity
+        return out
+
+
+class AttentionBlock(nn.Module):
+    """Self attention block.
+
+    Simplified variant from `"Learned Image Compression with
+    Discretized Gaussian Mixture Likelihoods and Attention Modules"
+    <https://arxiv.org/abs/2001.01568>`_, by Zhengxue Cheng, Heming Sun, Masaru
+    Takeuchi, Jiro Katto.
+
+    Args:
+        N (int): Number of channels)
+    """
+
+    def __init__(self, N):
+        super().__init__()
+
+        class ResidualUnit(nn.Module):
+            """Simple residual unit."""
+
+            def __init__(self):
+                super().__init__()
+                self.conv = nn.Sequential(
+                    conv1x1(N, N // 2),
+                    nn.ReLU(inplace=True),
+                    conv3x3(N // 2, N // 2),
+                    nn.ReLU(inplace=True),
+                    conv1x1(N // 2, N),
+                )
+                self.relu = nn.ReLU(inplace=True)
+
+            def forward(self, x):
+                identity = x
+                out = self.conv(x)
+                out += identity
+                out = self.relu(out)
+                return out
+
+        self.conv_a = nn.Sequential(ResidualUnit(), ResidualUnit(), ResidualUnit())
+
+        self.conv_b = nn.Sequential(
+            ResidualUnit(),
+            ResidualUnit(),
+            ResidualUnit(),
+            conv1x1(N, N),
+        )
+
+    def forward(self, x):
+        identity = x
+        a = self.conv_a(x)
+        b = self.conv_b(x)
+        out = a * torch.sigmoid(b)
+        out += identity
+        return out
+
+import math
+import torch
+import torch.nn as nn
+
+
+class HiLo(nn.Module):
+    """
+    HiLo Attention
+
+    Paper: Fast Vision Transformers with HiLo Attention
+    Link: https://arxiv.org/abs/2205.13213
+    """
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., window_size=2, alpha=0.5):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        head_dim = int(dim/num_heads)
+        self.dim = dim
+
+        # self-attention heads in Lo-Fi
+        self.l_heads = int(num_heads * alpha)
+        # token dimension in Lo-Fi
+        self.l_dim = self.l_heads * head_dim
+
+        # self-attention heads in Hi-Fi
+        self.h_heads = num_heads - self.l_heads
+        # token dimension in Hi-Fi
+        self.h_dim = self.h_heads * head_dim
+
+        # local window size. The `s` in our paper.
+        self.ws = window_size
+
+        if self.ws == 1:
+            # ws == 1 is equal to a standard multi-head self-attention
+            self.h_heads = 0
+            self.h_dim = 0
+            self.l_heads = num_heads
+            self.l_dim = dim
+
+        self.scale = qk_scale or head_dim ** -0.5
+
+        # Low frequence attention (Lo-Fi)
+        if self.l_heads > 0:
+            if self.ws != 1:
+                self.sr = nn.AvgPool2d(kernel_size=window_size, stride=window_size)
+            self.l_q = nn.Linear(self.dim, self.l_dim, bias=qkv_bias)
+            self.l_kv = nn.Linear(self.dim, self.l_dim * 2, bias=qkv_bias)
+            self.l_proj = nn.Linear(self.l_dim, self.l_dim)
+
+        # High frequence attention (Hi-Fi)
+        if self.h_heads > 0:
+            self.h_qkv = nn.Linear(self.dim, self.h_dim * 3, bias=qkv_bias)
+            self.h_proj = nn.Linear(self.h_dim, self.h_dim)
+
+    def hifi(self, x):
+        B, H, W, C = x.shape
+        h_group, w_group = H // self.ws, W // self.ws
+
+        total_groups = h_group * w_group
+
+        x = x.reshape(B, h_group, self.ws, w_group, self.ws, C).transpose(2, 3)
+
+        qkv = self.h_qkv(x).reshape(B, total_groups, -1, 3, self.h_heads, self.h_dim // self.h_heads).permute(3, 0, 1, 4, 2, 5)
+        q, k, v = qkv[0], qkv[1], qkv[2]  # B, hw, n_head, ws*ws, head_dim
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale  # B, hw, n_head, ws*ws, ws*ws
+        attn = attn.softmax(dim=-1)
+        attn = (attn @ v).transpose(2, 3).reshape(B, h_group, w_group, self.ws, self.ws, self.h_dim)
+        x = attn.transpose(2, 3).reshape(B, h_group * self.ws, w_group * self.ws, self.h_dim)
+
+        x = self.h_proj(x)
+        return x
+
+    def lofi(self, x):
+        B, H, W, C = x.shape
+
+        q = self.l_q(x).reshape(B, H * W, self.l_heads, self.l_dim // self.l_heads).permute(0, 2, 1, 3)
+
+        if self.ws > 1:
+            x_ = x.permute(0, 3, 1, 2)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            kv = self.l_kv(x_).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.l_kv(x).reshape(B, -1, 2, self.l_heads, self.l_dim // self.l_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, H, W, self.l_dim)
+        x = self.l_proj(x)
+        return x
+
+    def forward(self, x, H, W):
+        B, C, H, W = x.shape
+
+        x = x.reshape(B, H, W, C)
+
+        if self.h_heads == 0:
+            x = self.lofi(x)
+            return x.reshape(B, C, H, W)
+
+        if self.l_heads == 0:
+            x = self.hifi(x)
+            return x.reshape(B, C, H, W)
+
+        hifi_out = self.hifi(x)
+        lofi_out = self.lofi(x)
+
+        x = torch.cat((hifi_out, lofi_out), dim=-1)
+        x = x.reshape(B, H, W, C)
+
+        return x
+
+    def flops(self, H, W):
+        # pad the feature map when the height and width cannot be divided by window size
+        Hp = self.ws * math.ceil(H / self.ws)
+        Wp = self.ws * math.ceil(W / self.ws)
+
+        Np = Hp * Wp
+
+        # For Hi-Fi
+        # qkv
+        hifi_flops = Np * self.dim * self.h_dim * 3
+        nW = (Hp // self.ws) * (Wp // self.ws)
+        window_len = self.ws * self.ws
+        # q @ k and attn @ v
+        window_flops = window_len * window_len * self.h_dim * 2
+        hifi_flops += nW * window_flops
+        # projection
+        hifi_flops += Np * self.h_dim * self.h_dim
+
+        # for Lo-Fi
+        # q
+        lofi_flops = Np * self.dim * self.l_dim
+        kv_len = (Hp // self.ws) * (Wp // self.ws)
+        # k, v
+        lofi_flops += kv_len * self.dim * self.l_dim * 2
+        # q @ k and attn @ v
+        lofi_flops += Np * self.l_dim * kv_len * 2
+        # projection
+        lofi_flops += Np * self.l_dim * self.l_dim
+
+        return hifi_flops + lofi_flops
+
+if __name__ == '__main__':
+    block = HiLo(dim=128)
+    input = torch.rand(1,128, 16, 16)
+    output = block(input, 16, 16)
+    print(input.size())
+    print(output.size())
---
+++ TIM/compressai/version.py
@@ -0,0 +1,2 @@
+__version__ = "1.1.1"
+git_version = "cc68eac111a2c13579bf88871508e52de75c1950"
